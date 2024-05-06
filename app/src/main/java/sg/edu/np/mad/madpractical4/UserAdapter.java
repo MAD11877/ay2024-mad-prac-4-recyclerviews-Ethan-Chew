9312e7faf6569c9293268e82943ee66dcabbbac3
Ethan-Chew <ethanchewmh@gmail.com>
2024-05-06 17:26:18 +0800

    feat: completed practical 4
    Implemented RecyclerView, show image when ID contains 7

--- app/src/main/java/sg/edu/np/mad/madpractical4/UserAdapter.java
+++ app/src/main/java/sg/edu/np/mad/madpractical4/UserAdapter.java
@@ -7,22 +7,20 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.io.Console;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+
 
 public class UserAdapter extends RecyclerView.Adapter<UserViewHolder> {
     private ArrayList<User> listObjects;
+    private ListActivity activity;
 
     public UserAdapter(ArrayList<User> listObjects, ListActivity activity) {
         this.listObjects = listObjects;
+        this.activity = activity;
     }
 
     public UserViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -35,38 +33,33 @@ public class UserAdapter extends RecyclerView.Adapter<UserViewHolder> {
         User listItem = listObjects.get(position);
         holder.name.setText(listItem.getName());
         holder.description.setText(listItem.getDescription());
-
+        // Display Dialog when Image Clicked
         holder.smallImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println(listItem.getName());
                 AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
                 builder.setTitle("Profile")
                         .setMessage(listItem.getName())
-                        .setCancelable(true)
-                        .setPositiveButton("View", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Random rand = new Random();
-                                int randomNum = rand.nextInt(999999);
-                                // Send Random Number to MainActivity as an Intent
-                                Intent sendUser = new Intent(v.getContext(), MainActivity.class);
-                                sendUser.putExtra("name", listItem.getName());
-                                sendUser.putExtra("description", listItem.getDescription());
-                                sendUser.putExtra("followed", listItem.getFollowed());
-                                sendUser.putExtra("id", listItem.getId());
-                                v.getContext().startActivity(sendUser);
-                            }
+                        .setPositiveButton("Close", (dialog, which) -> {
+                            dialog.dismiss();
                         })
-                        .setNegativeButton("Close", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-
-                            }
+                        .setNegativeButton("View", (dialog, which) -> {
+                            Intent intent = new Intent(v.getContext(), MainActivity.class);
+                            intent.putExtra("id", listItem.getId());
+                            intent.putExtra("name", listItem.getName());
+                            intent.putExtra("description", listItem.getDescription());
+                            intent.putExtra("followed", listItem.getFollowed());
+                            v.getContext().startActivity(intent);
                         });
-                AlertDialog alert = builder.create();
+
+                builder.create().show();
             }
         });
+        // Check if ID contains 7, if yes, show image
+        String userId = String.valueOf(listItem.getId());
+        if (userId.contains("7")) {
+            holder.bigImage.setVisibility(View.VISIBLE);
+        }
     }
 
     public int getItemCount() { return listObjects.size(); }
--- app/src/main/java/sg/edu/np/mad/madpractical4/UserViewHolder.java
+++ app/src/main/java/sg/edu/np/mad/madpractical4/UserViewHolder.java
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class UserViewHolder extends RecyclerView.ViewHolder {
     ImageView smallImage;
+    ImageView bigImage;
     TextView name;
     TextView description;
 
@@ -15,6 +16,7 @@ public class UserViewHolder extends RecyclerView.ViewHolder {
         super(itemView);
 
         smallImage = itemView.findViewById(R.id.imageView);
+        bigImage = itemView.findViewById(R.id.largeImageView);
         name = itemView.findViewById(R.id.nameLabel);
         description = itemView.findViewById(R.id.descriptionLabel);
     }
